de84338e243fd232b4acb8772c52d7d107298b54
Haozhong Zhang <hzzhan9@gmail.com>
2018-11-09 20:49:40 +0800

    BrokerRegistry: switch to function specifications

--- BrokerRegistry.v
+++ BrokerRegistry.v
@@ -9,137 +9,167 @@ Require Import
         States
         Types.
 
+Module BrokerRegistry.
 
-Section Aux.
+  Section Aux.
 
-  Definition get_brokersMap (wst: WorldState) : BrokersMap.t :=
-    broker_registry_brokersMap (wst_broker_registry_state wst).
+    Definition get_brokersMap (wst: WorldState) : BrokersMap.t :=
+      broker_registry_brokersMap (wst_broker_registry_state wst).
 
-  Definition get_broker_info
-             (m: BrokersMap.t) (owner broker: address)
-    : option Broker :=
-    match BrokersMap.find m owner with
-    | None => None
-    | Some brokers =>
-      match Brokers.find brokers broker with
+    Definition get_broker_info
+               (m: BrokersMap.t) (owner broker: address)
+      : option Broker :=
+      match BrokersMap.find m owner with
       | None => None
-      | Some broker_info => Some broker_info
-      end
+      | Some brokers =>
+        match Brokers.find brokers broker with
+        | None => None
+        | Some broker_info => Some broker_info
+        end
+      end.
+
+    Definition upd_broker_info
+               (m: BrokersMap.t) (owner broker: address) (broker_info: Broker)
+      : BrokersMap.t :=
+      BrokersMap.upd m
+                     owner
+                     (Brokers.upd (BrokersMap.get m owner)
+                                  broker
+                                  broker_info).
+
+    Definition del_broker_info
+               (m: BrokersMap.t) (owner broker: address)
+      : BrokersMap.t :=
+      BrokersMap.upd m
+                     owner
+                     (Brokers.del (BrokersMap.get m owner) broker).
+
+  End Aux.
+
+  Section GetBroker.
+
+    Definition getBroker_spec (sender owner broker: address) :=
+      {|
+        fspec_require :=
+          fun wst => True
+        ;
+
+        fspec_trans :=
+          fun wst wst' retval =>
+            wst' = wst /\
+            retval = match get_broker_info (get_brokersMap wst) owner broker with
+                     | None => RetGetBroker false 0
+                     | Some broker_info => RetGetBroker true (broker_interceptor broker_info)
+                     end
+        ;
+
+        fspec_events :=
+          fun wst events => events = nil
+        ;
+      |}.
+
+  End GetBroker.
+
+  Section RegisterBroker.
+
+    Definition registerBroker_spec (sender broker interceptor: address) :=
+      {|
+        fspec_require :=
+          fun wst =>
+            broker <> 0 /\
+            get_broker_info (get_brokersMap wst) sender broker = None;
+
+        fspec_trans :=
+          fun wst wst' retval =>
+            let brokerInfo := {| broker_owner := sender;
+                                 broker_addr := broker;
+                                 broker_interceptor := interceptor;
+                              |} in
+            let brokersMap' := upd_broker_info
+                                 (get_brokersMap wst) sender broker brokerInfo in
+            wst' = wst_update_broker_registry
+                     wst {| broker_registry_brokersMap := brokersMap' |} /\
+            retval = RetNone;
+
+        fspec_events :=
+          fun wst events =>
+            events = EvtBrokerRegistered sender broker interceptor :: nil;
+      |}.
+
+  End RegisterBroker.
+
+  Section UnregisterBroker.
+
+    Definition unregisterBroker_spec (sender broker: address) :=
+      {|
+        fspec_require :=
+          fun wst =>
+            broker <> 0 /\
+            get_broker_info (get_brokersMap wst) sender broker <> None;
+
+        fspec_trans :=
+          fun wst wst' retval =>
+            get_broker_info (get_brokersMap wst) sender broker <> None ->
+            let brokersMap' := del_broker_info (get_brokersMap wst) sender broker in
+            wst' = wst_update_broker_registry
+                     wst
+                     {| broker_registry_brokersMap := brokersMap' |} /\
+            retval = RetNone;
+
+        fspec_events :=
+          fun wst events =>
+            forall broker_info,
+              get_broker_info (get_brokersMap wst) sender broker = Some broker_info ->
+              events = EvtBrokerUnregistered sender broker (broker_interceptor broker_info) :: nil;
+      |}.
+
+  End UnregisterBroker.
+
+  Section UnregisterAllBrokers.
+
+    Definition unregisterAllBrokers_spec (sender: address) :=
+      {|
+        fspec_require :=
+          fun wst => True;
+
+        fspec_trans :=
+          fun wst wst' retval =>
+            let brokersMap' := BrokersMap.del (get_brokersMap wst) sender in
+            wst' = wst_update_broker_registry
+                     wst {| broker_registry_brokersMap := brokersMap' |} /\
+            retval = RetNone;
+
+        fspec_events :=
+          fun wst events =>
+            events = EvtAllBrokersUnregistered sender :: nil;
+      |}.
+
+  End UnregisterAllBrokers.
+
+  Definition get_spec
+             (msg: BrokerRegistryMsg)
+    : FSpec :=
+    match msg with
+    | msg_getBroker sender owner broker =>
+      getBroker_spec sender owner broker
+
+    | msg_registerBroker sender broker interceptor =>
+      registerBroker_spec sender broker interceptor
+
+    | msg_unregisterBroker sender broker =>
+      unregisterBroker_spec sender broker
+
+    | msg_unregisterAllBrokers sender =>
+      unregisterAllBrokers_spec sender
     end.
 
-  Definition upd_broker_info
-             (m: BrokersMap.t) (owner broker: address) (broker_info: Broker)
-    : BrokersMap.t :=
-    BrokersMap.upd m
-                   owner
-                   (Brokers.upd (BrokersMap.get m owner)
-                                broker
-                                broker_info).
+  Definition model
+             (wst: WorldState)
+             (msg: BrokerRegistryMsg)
+             (wst': WorldState)
+             (retval: RetVal)
+             (events: list Event)
+    : Prop :=
+    fspec_sat (get_spec msg) wst wst' retval events.
 
-  Definition del_broker_info
-             (m: BrokersMap.t) (owner broker: address)
-    : BrokersMap.t :=
-    BrokersMap.upd m
-                   owner
-                   (Brokers.del (BrokersMap.get m owner) broker).
-
-End Aux.
-
-
-Section Func_getBroker.
-
-  Definition func_getBroker
-             (wst0 wst: WorldState) (sender owner broker: address)
-  : (WorldState * RetVal * list Event) :=
-    let brokersMap := get_brokersMap wst in
-    match get_broker_info brokersMap owner broker with
-    | None => (wst, RetGetBroker false 0, nil)
-    | Some broker_info => (wst,
-                          RetGetBroker true (broker_interceptor broker_info),
-                          nil)
-    end.
-
-End Func_getBroker.
-
-
-Section Func_registerBroker.
-
-  Definition func_registerBroker
-             (wst0 wst: WorldState) (sender broker interceptor: address)
-  : (WorldState * RetVal * list Event) :=
-    match broker with
-    | O => (wst0, RetNone, EvtRevert :: nil)
-    | _ =>
-      let brokersMap := get_brokersMap wst in
-      match get_broker_info brokersMap sender broker with
-      | Some _ => (wst0, RetNone, EvtRevert :: nil)
-      | None =>
-        let brokerInfo := {| broker_owner := sender;
-                             broker_addr := broker;
-                             broker_interceptor := interceptor;
-                          |}
-        in
-        let brokersMap' := upd_broker_info brokersMap sender broker brokerInfo in
-        (wst_update_broker_registry wst {| broker_registry_brokersMap := brokersMap' |},
-         RetNone,
-         EvtBrokerRegistered sender broker interceptor :: nil
-        )
-      end
-    end.
-
-End Func_registerBroker.
-
-
-Section Func_unregisterBroker.
-
-  Definition func_unregisterBroker
-             (wst0 wst: WorldState) (sender broker: address)
-  : WorldState * RetVal * list Event :=
-    match broker with
-    | O => (wst0, RetNone, EvtRevert :: nil)
-    | _ =>
-      let brokersMap := get_brokersMap wst in
-      match get_broker_info brokersMap sender broker with
-      | None => (wst0, RetNone, EvtRevert :: nil)
-      | Some broker_info =>
-        let brokersMap' := del_broker_info brokersMap sender broker in
-        (wst_update_broker_registry wst {| broker_registry_brokersMap := brokersMap' |},
-         RetNone,
-         EvtBrokerUnregistered sender broker (broker_interceptor broker_info) :: nil
-        )
-      end
-    end.
-
-End Func_unregisterBroker.
-
-
-Section Func_unregsterAllBrokers.
-
-  Definition func_unregisterAllBrokers
-             (wst0 wst: WorldState) (sender: address)
-  : WorldState * RetVal * list Event :=
-    let brokersMap := get_brokersMap wst in
-    let brokersMap' := BrokersMap.del brokersMap sender in
-    (wst_update_broker_registry wst {| broker_registry_brokersMap := brokersMap' |},
-     RetNone,
-     EvtAllBrokersUnregistered sender :: nil
-    ).
-
-End Func_unregsterAllBrokers.
-
-
-Definition BrokerRegistry_step
-           (wst0 wst: WorldState) (msg: BrokerRegistryMsg)
-  : (WorldState * RetVal * list Event) :=
-  match msg with
-  | msg_getBroker sender owner broker =>
-    func_getBroker wst0 wst sender owner broker
-  (* | msg_getBrokers sender owner s e => *)
-  (*   func_getBrokers wst0 wst sender owner s e *)
-  | msg_registerBroker sender broker interceptor =>
-    func_registerBroker wst0 wst sender broker interceptor
-  | msg_unregisterBroker sender broker =>
-    func_unregisterBroker wst0 wst sender broker
-  | msg_unregisterAllBrokers sender =>
-    func_unregisterAllBrokers wst0 wst sender
-  end.
+End BrokerRegistry.
--- LibModel.v
+++ LibModel.v
@@ -8,3 +8,18 @@ Require Import
 
 Set Implicit Arguments.
 Unset Strict Implicit.
+
+
+Record FSpec : Type :=
+  mk_fspec {
+      fspec_require: WorldState -> Prop;
+      fspec_trans: WorldState -> WorldState -> RetVal -> Prop;
+      fspec_events: WorldState -> list Event -> Prop;
+    }.
+
+Definition fspec_sat
+           (fspec: FSpec) (wst wst': WorldState) (retval: RetVal) (events: list Event)
+  : Prop :=
+  fspec_require fspec wst /\
+  fspec_trans fspec wst wst' retval /\
+  fspec_events fspec wst events.
