f1e53d3c9729dccc4a570601483dc4c58b94211d
Haozhong Zhang <hzzhan9@gmail.com>
2018-11-10 18:01:50 +0800

    LibModel: add comments for FSpec

--- LibModel.v
+++ LibModel.v
@@ -5,9 +5,23 @@ Require Import
 
 Record FSpec : Type :=
   mk_fspec {
-      fspec_require: WorldState -> Prop;
-      fspec_trans: WorldState -> WorldState -> RetVal -> Prop;
-      fspec_events: WorldState -> list Event -> Prop;
+      (* Requirements that must be satisfied by the pre-state in order
+         to execute a message call successfully (i.e., not revert event
+         occurs). *)
+      fspec_require: WorldState (* pre-state *) ->
+                     Prop;
+
+      (* Relations between the pre-state and the post-state of a
+         successful execution of a message call. *)
+      fspec_trans: WorldState (* pre-state *) ->
+                   WorldState (* post-state *) ->
+                   RetVal -> Prop;
+
+      (* Propositions that must be satisfied by the events generated
+         from a successful execution of a message call. *)
+      fspec_events: WorldState (* pre-state *) ->
+                    list Event (* events *) ->
+                    Prop;
     }.
 
 Definition fspec_sat
