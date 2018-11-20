d8c820b93c0bafc2a5b564c3c14c8d630d2494fb
Hanru Jiang <hanru219@mail.ustc.edu.cn>
2018-11-20 19:30:58 +0800

    BurnRateTable.v almost finished. Some definitions need added to States.v/Messages.v/Events.v/ERC20.v

--- BurnRateTable.v
+++ BurnRateTable.v
@@ -9,8 +9,11 @@ Require Import
         States
         Types.
 
-(** * BurnRateTable messages *)
+Require Import
+        ERC20.
 
+(** * BurnRateTable messages *)
+(** TODO: move to Messages.v *)
 Inductive BurnRateTableMsg : Type :=
 | msg_getBurnRate (sender: address) (token: address)
 | msg_getTokenTier (sender: address) (token: address)
@@ -24,7 +27,7 @@ Inductive BurnRateTableMsg : Type :=
    are not implemented *).
 
 (** * BurnRateTable state *)
-
+(** TODO: move to State.v *)
 (** Tier definition *)
 Definition TIER1 : uint := 3.
 Definition TIER2 : uint := 2.
@@ -71,10 +74,14 @@ Module TokenDataMap := Mapping Address_as_DT TokenDataElem.
 
 (** BurnRateTable state *)
 Record BurnRateTableState : Type :=
-  {
+  mk_burn_rate_table_state {
     burnratetable_tokens: TokenDataMap.t;
     (* balances not implemented *)
-  }.
+    }.
+
+(** * Event *)
+(** TODO: move to Events.v *)
+Parameter EvtTokenTierUpgraded : forall (token: address) (TIER: uint), Event.
 
 (** * Constants *)
 Definition BURN_BASE_PERCENTAGE : uint := 100 * 10. (* 100% *)
@@ -91,22 +98,211 @@ Definition BURN_P2P_TIER2 : uint := 2 * 10. (* 2% *)
 Definition BURN_P2P_TIER3 : uint := 3 * 10. (* 3% *)
 Definition BURN_P2P_TIER4 : uint := 6 * 10. (* 6% *)
 
+(* Defining 0x10000 in nat causes stack overflow... *)
+Parameter x10000 : uint.
+
+Parameter YEAR_TO_SECONDS : uint.
+
+(** Here I assumed the BurnRateTable is constructed using correct LRC and WETH addresses *)    
+(* LRC address, WETH address *)
+Parameter lrcAddress : address.
+Parameter wethAddress : address.
+
 (** * Auxiliary definitions *)
-(** What about events? should it be inserted in Events definition? *)
-Parameter EvtTokenTierUpgraded : forall (addr: address) (tier: uint), Event.
+
 (** It seems the [BurnRateTableState] should be added to WorldState. *)
 Parameter get_state : WorldState -> BurnRateTableState.
 Parameter set_state : WorldState -> BurnRateTableState -> WorldState.
 
 (** * Method call specs *)
-Parameter getBurnRate_spec : forall (sender: address) (token: address), FSpec.
-Parameter getTokenTier_spec : forall (sender: address) (token: address), FSpec.
-Parameter upgradeTokenTier_spec : forall (sender: address) (token: address), FSpec.
+
+Section getTokenTier_SPEC.
+
+  Variable (sender: address).
+  Variable (token: address).
+
+  (* function body: 
+     function getTokenTier(
+        address token
+        )
+        public
+        view
+        returns (uint tier)
+    {
+        TokenData storage tokenData = tokens[token];
+        // Fall back to lowest tier
+        tier = (now > tokenData.validUntil) ? TIER_4 : tokenData.tier;
+    }
+   *)
+
+  Definition getTIER (wst: WorldState) : uint :=
+    let tokens := (burnratetable_tokens (get_state wst)) in
+    let data := TokenDataMap.get tokens token in
+    if Nat.ltb data.(validUntil) (block_timestamp (wst_block_state wst))
+    then TIER4
+    else data.(tier).
+      
+  Definition getTokenTier_require : WorldState -> Prop := fun _ => True.
+
+  Inductive getTokenTier_trans : WorldState -> WorldState -> RetVal -> Prop :=
+  | getTokenTierTrans: forall wst, getTokenTier_trans wst wst (RetUint (getTIER wst)).
+
+  Inductive getTokenTier_events : WorldState -> list Event -> Prop :=
+  | getTokenTierEvents: forall wst, getTokenTier_events wst nil.
+
+  Definition getTokenTier_spec : FSpec :=
+    mk_fspec getTokenTier_require getTokenTier_trans getTokenTier_events.
+  
+End getTokenTier_SPEC.
+
+
+Section getBurnRate_SPEC.
+  Variable (sender: address).
+  Variable (token: address).
+
+  (* function body:
+     function getBurnRate(
+        address token
+        )
+        external
+        view
+        returns (uint32 burnRate)
+    {
+        uint tier = getTokenTier(token);
+        if (tier == TIER_1) {
+            burnRate = uint32(BURN_P2P_TIER1) * 0x10000 + BURN_MATCHING_TIER1;
+        } else if (tier == TIER_2) {
+            burnRate = uint32(BURN_P2P_TIER2) * 0x10000 + BURN_MATCHING_TIER2;
+        } else if (tier == TIER_3) {
+            burnRate = uint32(BURN_P2P_TIER3) * 0x10000 + BURN_MATCHING_TIER3;
+        } else {
+            burnRate = uint32(BURN_P2P_TIER4) * 0x10000 + BURN_MATCHING_TIER4;
+        }
+    }
+   *)
+  Definition getRate (TIER: uint) : uint :=
+    match TIER with
+    (* TIER 4 *)
+    | O => BURN_P2P_TIER4 * x10000 + BURN_MATCHING_TIER4
+    (* TIER 3 *)
+    | S O => BURN_P2P_TIER3 * x10000 + BURN_MATCHING_TIER3
+    (* TIER 2 *)   
+    | S (S O) => BURN_P2P_TIER2 * x10000 + BURN_MATCHING_TIER2
+    (* TIER 1 *)
+    | S (S (S O)) => BURN_P2P_TIER1 * x10000 + BURN_MATCHING_TIER1
+    (* other cases *)
+    | _ => BURN_P2P_TIER4 * x10000 + BURN_MATCHING_TIER4
+    end.
+  
+  Definition getBurnRate_require : WorldState -> Prop := fun _ => True.
+  
+  Inductive getBurnRate_trans : WorldState -> WorldState -> RetVal -> Prop :=
+  | getBurnRateTrans: forall wst,
+      getBurnRate_trans wst wst (RetUint (getRate (getTIER token wst))).
+  
+  Inductive getBurnRate_events : WorldState -> list Event -> Prop :=
+  | getBurnRateEvents: forall wst, getBurnRate_events wst nil.
+  
+  Definition getBurnRate_spec : FSpec :=
+    mk_fspec getBurnRate_require getBurnRate_trans getBurnRate_events.
+
+End getBurnRate_SPEC.
+
+
+Section upgradeTokenTier_SPEC.
+  
+  Variable (sender: address).
+  Variable (token: address).
+  (* function body 
+     function upgradeTokenTier(
+        address token
+        )
+        external
+        returns (bool)
+    {
+        require(token != 0x0, ZERO_ADDRESS);
+        require(token != lrcAddress, BURN_RATE_FROZEN);
+        require(token != wethAddress, BURN_RATE_FROZEN);
+
+        uint currentTier = getTokenTier(token);
+
+        // Can't upgrade to a higher level than tier 1
+        require(currentTier != TIER_1, BURN_RATE_MINIMIZED);
+
+        // Burn TIER_UPGRADE_COST_PERCENTAGE of total LRC supply
+        BurnableERC20 LRC = BurnableERC20(lrcAddress);
+        uint totalSupply = LRC.totalSupply();
+        uint amount = totalSupply.mul(TIER_UPGRADE_COST_PERCENTAGE) / BURN_BASE_PERCENTAGE;
+        bool success = LRC.burnFrom(msg.sender, amount);
+        require(success, BURN_FAILURE);
+
+        // Upgrade tier
+        TokenData storage tokenData = tokens[token];
+        tokenData.validUntil = now.add(2 * YEAR_TO_SECONDS);
+        tokenData.tier = currentTier + 1;
+
+        emit TokenTierUpgraded(token, tokenData.tier);
+
+        return true;
+    }
+   *)
+
+
+  Definition burnamount (totalSupply: uint) : uint :=
+    Nat.div (totalSupply * TIER_UPGRADE_COST_PERCENTAGE) BURN_BASE_PERCENTAGE.
+
+  Definition upgradetier (wst: WorldState) : WorldState * uint :=
+    let tokens := burnratetable_tokens (get_state wst) in
+    let data := TokenDataMap.get tokens token in
+    let NOW := (block_timestamp (wst_block_state wst)) in
+    let data' := mk_token_data (data.(tier) + 1) (NOW + 2 * YEAR_TO_SECONDS) in
+    let tokens' := TokenDataMap.upd tokens token data' in
+    (set_state wst (mk_burn_rate_table_state tokens'), data.(tier) + 1) .
+                                 
+  (* TODO: add burnFrom to ERC20 model *)
+  Parameter msg_burnFrom : forall (sender: address) (token: address) (amount: uint), ERC20Msg.
+  Definition upgradeTokenTier_require (wst: WorldState) : Prop :=
+    exists totalSupply wst' evts,
+      token <> 0
+      /\ token <> lrcAddress
+      /\ token <> wethAddress
+      /\ getTIER token wst <> TIER1
+      (* TODO: should [sender] be the caller or transaction origin? *)
+      (* TODO: does totalSupply generate ERC20 event? *)
+      (* TODO: what's the return value of burnFrom? *)
+      /\ ERC20s.model wst (msg_totalSupply sender lrcAddress)
+                     wst (RetUint totalSupply) nil
+      /\ ERC20s.model wst (msg_burnFrom sender lrcAddress (burnamount totalSupply))
+                     wst' (RetBool true) evts.
+  
+  Inductive upgradeTokenTier_trans (wst : WorldState) : WorldState -> RetVal -> Prop :=
+  | upgradeTokenTier_TRANS: forall totalSupply wst' evts TIER wst'' ,
+      ERC20s.model wst (msg_totalSupply sender lrcAddress)
+                   wst (RetUint totalSupply) nil ->
+      ERC20s.model wst (msg_burnFrom sender lrcAddress (burnamount totalSupply))
+                   wst' (RetBool true) evts ->
+      upgradetier wst' = (wst'', TIER) ->
+      upgradeTokenTier_trans wst wst'' (RetBool true).
+      
+  Inductive upgradeTokenTier_events (wst : WorldState) : list Event -> Prop :=
+  | upgradeTokenTier_EVENTS: forall totalSupply wst' evts TIER wst'' ,
+      ERC20s.model wst (msg_totalSupply sender lrcAddress)
+                   wst (RetUint totalSupply) nil ->
+      ERC20s.model wst (msg_burnFrom sender lrcAddress (burnamount totalSupply))
+                   wst' (RetBool true) evts ->
+      upgradetier wst' = (wst'', TIER) ->
+      upgradeTokenTier_events wst (evts ++ EvtTokenTierUpgraded token TIER :: nil).
+  
+  Definition upgradeTokenTier_spec : FSpec :=
+    mk_fspec upgradeTokenTier_require upgradeTokenTier_trans upgradeTokenTier_events.
+
+End upgradeTokenTier_SPEC.
+
 
 Definition get_spec (msg: BurnRateTableMsg) : FSpec :=
   match msg with
-  | msg_getBurnRate sender token => getBurnRate_spec sender token
-  | msg_getTokenTier sender token => getTokenTier_spec sender token
+  | msg_getBurnRate sender token => getBurnRate_spec token
+  | msg_getTokenTier sender token => getTokenTier_spec token
   | msg_upgradeTokenTier sender token => upgradeTokenTier_spec sender token
   end.
 
