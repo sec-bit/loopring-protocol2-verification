6c40fa0e17ec12147bb1bcb2281440b215b8a464
Haozhong Zhang <hzzhan9@gmail.com>
2018-11-29 16:50:09 +0800

    Props/FeeHolder: amend the correctness proof of withdrawBurned

--- Props/FeeHolderProps.v
+++ Props/FeeHolderProps.v
@@ -16,6 +16,25 @@ Require Import
         TopModel.
 
 
+Section Aux.
+
+  Definition get_feebalance (wst: WorldState) (token owner: address) : uint :=
+    AA2V.get (feeholder_feeBalances (wst_feeholder_state wst))
+             (token, owner).
+
+  Definition transfer_preserve_feebalance
+             (wst: WorldState) (token from to: address) (amount: uint) : Prop :=
+    forall wst' events,
+      FeeHolder.transfer_withdraw_succeed
+        (FeeHolder.wst_before_transfer wst from to token amount)
+        to token amount wst' events ->
+      get_feebalance
+        (FeeHolder.wst_before_transfer wst from to token amount) token from =
+      get_feebalance wst' token from.
+
+End Aux.
+
+
 Section WithdrawBurnedReqAuth.
 
   Theorem withdrawBurn_noauth:
@@ -34,16 +53,16 @@ Section WithdrawBurnedReqAuth.
     congruence.
   Qed.
 
-  Definition get_feebalance (wst: WorldState) (token owner: address) : uint :=
-    AA2V.get (feeholder_feeBalances (wst_feeholder_state wst))
-             (token, owner).
-
   Theorem withdrawBurned_auth:
     forall sender wst,
       FeeHolder.is_authorized wst sender ->
       forall token amount wst' retval events,
         lr_step wst (MsgFeeHolder (msg_withdrawBurned sender token amount))
                 wst' retval events ->
+        (transfer_preserve_feebalance
+           wst token (wst_feeholder_addr wst) sender amount ->
+         get_feebalance wst' token (wst_feeholder_addr wst) =
+         get_feebalance wst token (wst_feeholder_addr wst) - amount) /\
         retval = RetBool true /\
         In (EvtTokenWithdrawn (wst_feeholder_addr wst) token amount) events.
   Proof.
@@ -54,11 +73,21 @@ Section WithdrawBurnedReqAuth.
     destruct Hreq as [[Hamount Htransfer] _].
     destruct Htransfer as [wst'' [events' Htransfer]].
 
-    split.
-    - specialize (Htrans wst'' events' Htransfer).
-      destruct Htrans as [Hwst' Hretval].
-      auto.
-    - specialize (Hevents wst'' events' Htransfer).
+    specialize (Htrans wst'' events' Htransfer).
+    destruct Htrans as [Hwst' Hretval].
+    subst wst''; subst retval.
+
+    split; auto.
+    - intros Hpreserve.
+      specialize (Hpreserve wst' events' Htransfer).
+      unfold get_feebalance in *; simpl in *.
+      rewrite <- Hpreserve.
+      unfold AA2V_upd_dec; simpl.
+      rewrite AA2V.get_upd_eq; auto.
+      rewrite (minus_safe _ _ Hamount); auto.
+
+    - split; auto.
+      specialize (Hevents wst' events' Htransfer).
       rewrite Hevents.
       apply in_or_app; right.
       constructor; auto.
